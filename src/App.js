fa45f27100291d1d893ce728c72fd38ca44445ff
shinequasar <shinequasar@gmail.com>
2022-09-16 12:39:22 +0900

    [Add]twin.micro 세팅(tailwind-css + Styled components)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import CategoryPage from "./pages/CategoryPage";
+import BoardPage from "./pages/BoardPage";
+import MyPage from "./pages/MyPage";
+
 function App() {
   return (
     <div className="App">
+
+        <Routes>
+            <Route path="/" element={<MainPage/>}></Route>
+            <Route path="/category" element={<CategoryPage/>}></Route>
+            <Route path="/board" element={<BoardPage/>}></Route>
+            <Route path="/mypage" element={<MyPage/>}></Route>
+            <Route path="*" element={<div>없는 페이지입니다.</div>}/>
+        </Routes>
 
     </div>
   );
